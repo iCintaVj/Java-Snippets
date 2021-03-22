14e0c81bdd46055d300a670783fa705da86e2d50
Vijay <36810640+iCintaVj@users.noreply.github.com>
2021-03-22 22:32:15 +0530

    Update AESEncryption.java

--- AESEncryption.java
+++ AESEncryption.java
@@ -1,9 +1,14 @@
-
 import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+
 import com.sun.management.OperatingSystemMXBean;
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.Base64;
-
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
@@ -14,37 +19,28 @@ import javax.management.ObjectName;
 import javax.xml.bind.DatatypeConverter;
 
 public class Test {
-
-	private static final int encryptionKeyLength = 100;
+	private static final ExecutorService pool = Executors.newFixedThreadPool(3);
+	private static  int encryptionKeyLength;
+	private static  int encryptionBits;
+	private static  int messageSize;
 	private static String encryptionKey = "";
+	private static String plainText = "";
 	private static final String characterEncoding = "UTF-8";
 	private static final String cipherTransformation = "AES/CBC/PKCS5PADDING";
 	private static final String aesEncryptionAlgorithem = "AES";
 
-	static{
-		for (int i=0; i < encryptionKeyLength; i++) {
-			encryptionKey += "1";
-		}
-	}
-
 	public static String encrypt(String plainText) {
 		String encryptedText = "";
 		try {
-			
-			KeyGenerator keygenerator 
-            = KeyGenerator.getInstance("AES"); 
-        keygenerator.init(256, new SecureRandom(encryptionKey.getBytes())); 
-        SecretKey generatedKey =keygenerator.generateKey(); 
-			
+			KeyGenerator keygenerator = KeyGenerator.getInstance("AES");
+			keygenerator.init(encryptionBits, new SecureRandom(encryptionKey.getBytes()));
+			SecretKey generatedKey = keygenerator.generateKey();
 			Cipher cipher = Cipher.getInstance(cipherTransformation);
-			byte[] key = encryptionKey.getBytes(characterEncoding);
-			SecretKeySpec secretKey = new SecretKeySpec(key, aesEncryptionAlgorithem);
 			IvParameterSpec ivparameterspec = new IvParameterSpec(new byte[16]);
 			cipher.init(Cipher.ENCRYPT_MODE, generatedKey, ivparameterspec);
 			byte[] cipherText = cipher.doFinal(plainText.getBytes("UTF8"));
 			Base64.Encoder encoder = Base64.getEncoder();
 			encryptedText = encoder.encodeToString(cipherText);
-
 		} catch (Exception E) {
 			System.err.println("Encrypt Exception : " + E.getMessage());
 		}
@@ -54,16 +50,10 @@ public class Test {
 	public static String decrypt(String encryptedText) {
 		String decryptedText = "";
 		try {
-			
-			KeyGenerator keygenerator 
-            = KeyGenerator.getInstance("AES"); 
-  
-        keygenerator.init(256, new SecureRandom(encryptionKey.getBytes())); 
-        SecretKey generatedKey =keygenerator.generateKey(); 
-			
+			KeyGenerator keygenerator = KeyGenerator.getInstance("AES");
+			keygenerator.init(encryptionBits, new SecureRandom(encryptionKey.getBytes()));
+			SecretKey generatedKey = keygenerator.generateKey();
 			Cipher cipher = Cipher.getInstance(cipherTransformation);
-			byte[] key = encryptionKey.getBytes(characterEncoding);
-			SecretKeySpec secretKey = new SecretKeySpec(key, aesEncryptionAlgorithem);
 			IvParameterSpec ivparameterspec = new IvParameterSpec(new byte[16]);
 			cipher.init(Cipher.DECRYPT_MODE, generatedKey, ivparameterspec);
 			Base64.Decoder decoder = Base64.getDecoder();
@@ -76,41 +66,67 @@ public class Test {
 		return decryptedText;
 	}
 
-	public static void test(int keyLength) throws Exception {
-		String plainText = "The quick brown fox jumps over the lazy dog.";
-		long startTime = System.currentTimeMillis(), endTime = 0L;
-		String encyptStr = encrypt(plainText);
-		endTime = System.currentTimeMillis();
+	private static void printStats(String type) {
+		OperatingSystemMXBean operatingSystemMXBean = ((OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean());
 
-		System.out.println("Time taken to perform encryption is :" + (endTime - startTime));
+		double currentCpu = operatingSystemMXBean.getProcessCpuLoad();
+		double availableProcessors = operatingSystemMXBean.getAvailableProcessors();
 
-		startTime = System.currentTimeMillis();
-		String cipherText = decrypt(encyptStr);
-		endTime = System.currentTimeMillis();
+		System.out.println("CPU Used : " + type + (currentCpu / availableProcessors) * 100);
 
-		System.out.println("Time taken to perform decryption is :" + (endTime - startTime));
+		double freeMemory = (double) operatingSystemMXBean.getFreePhysicalMemorySize();
+		double totalMemory = (double) operatingSystemMXBean.getTotalPhysicalMemorySize();
+		System.out.println("Memory Used : " + type + (double) (((totalMemory - freeMemory) / totalMemory) * 100));
+
+		System.out.println("Heap Memory Used : " + type + ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getUsed());
+		System.out.println("Non Heap Memory Used : " + type + ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage().getUsed());
+
+	}
+
+	private static void test(int keyLength, String plainText) {
+
+//		pool.submit(new Thread(new Runnable() {
+//			@Override
+//			public void run() {
+				long startTime = System.currentTimeMillis(), endTime = 0L;
+				String encyptStr = encrypt(plainText);
+				endTime = System.currentTimeMillis();
+				long timeTaken = (endTime - startTime);
+				System.out.println("Time taken to perform encryption is :" + timeTaken);
+				printStats("Encrypt : ");
+//			}
+//		}));
+
+		// pool.submit(new Thread(new Runnable() {
+		// @Override
+		// public void run() {
+		// long startTime = System.currentTimeMillis(),
+		// endTime = 0L;
+		// String cipherText =
+		// decrypt("x/0YNcigt6KxWBy3QJeG1+qFJVZXZ/LWvGG6FD3VVaKlN3WMqqnDxgpsVAQBlNilXll1T5mEs5257XDLUXTAJTGz6pttf2xYb+A3K+U9B0ZGoRPmew0S1YjTGAMGEZvLBg27xEz7LZeK+HPiXOQnPozcCrK3BgJHvcrQoq0O/ofsTiIVVSJm2OnQ8Y7FuJlkFkaG/coowk+hOO5f05i8vV3GU5mFG1EH232rc/13YfywwTn+csCz0qQkUpC9DgwRLT2Fetx2nx1e6vhphEA6T/WNQCEd5g809QR2uUP7uKjweCa89Ky7CHVRvropE4lo4wu3Z3zxODMFkJWC9M4iE+ee31lXvQNEnvej/HWjDIs6KAfoCvz0w95GlCGJkQSnuNU5C1nEBvV0HDrjRgbpQSo93dF9wB9wmO+jXTh7Dw68Hro5Ih438dvav2vS283yRMS5sKCXAS15hLtOfSU+dNbAJAUt+g8SZskQc+y3U4yf9A7EOP46R9+yARypu/Aicg3vpMGGRAMLfCjek+JFrX/sTkLH5AtNrwTQzuG3ddcdojc7xBBX2IfvAQ8m14yREeJZv45xxphn/2t40guYtHw++THU6wm/fchbyD58AKhThtA+hPA0LYL0jGEQxKMVUa/dbclx+MGbj+u4IuFmphhNvPEln9VwkmwebsGuM2kI6aSvGI30OVwU1c+/NR/KQ9ftUQGYTCddQtVK0y4+GloBKmzUpfd3ar7Sgz8etDzyc53l4p6zSjztZ69wxrlMV/W+H7RM2X+TRQ0o5YxNIPMdulTiSKSZb1n9oHyJ2ORwXlqP61n6O838JYeBl5fXN7s/cfVTBj0uvldjEvlNFN5RHMTWwSkioxuJD8Z0vSzJ4zG9nqkhfBS96JbPADVsYAluj4Ji9q9CELoH1UFe/zyXB8RL5oQkPmSRcZrjpQpvT1zDZpbaDrckj4N28y780kICYrz11S/sLSDA2CkO0s1uUkKIY05BQw8SIfHeD8KBNxvXWEsCo4E81lUHgiXzUW/TX0d3+QRNPOEAmK3WeZhOV8ZygR70G8b8m1rKOz6YMzeiXxgrKTsqi1LWp1ibXL92VfiUIrF8YwovOhYV6HhpOiEA959i4yBwWvt9YTlquMNU9tAKShWqlDTUKX7rtadX5X9x6XItbOwlGFWdw7Lmre/xWpKeO/c6AngtllG/f/xY7jJxIBYAuXj5DiE8G16jAQRqtupVbYVBVYWkDfZjH7bInAnMs/VSgFv9eRAYpryLtz+BGnDHsOk27cX0C/MS9XyNX4Igiai0tMJShVJ6pTp1rQGO4SbNIE7OdSFXw5gHSH3BF5Nhe9PHj3jXoEGqvJ4aDrYw6jNRGHlGcGA8KK0FEgDEk0EO8qJI7lc=");
+		// endTime = System.currentTimeMillis();
+		// long timeTaken = (endTime - startTime);
+		// System.out.println("Time taken to perform decryption is :" + timeTaken);
+		// printStats("Decrypt : ");
+		// }
+		// }));
+
 	}
 
 	public static void main(String args[]) throws Exception {
-		test(128);
-//		 test(192);
-//		 test(256);
+		
+		encryptionKeyLength = Integer.parseInt(args[0]);
+		encryptionBits = Integer.parseInt(args[1]);
+		messageSize = Integer.parseInt(args[2]);
 
-		OperatingSystemMXBean operatingSystemMXBean = ((OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean());
+		char[] chars = new char[messageSize * 1000000];
+		Arrays.fill(chars, 'V');
+		plainText = new String(chars);
 
-		double currentCpu = operatingSystemMXBean.getSystemLoadAverage();
-		int availableProcessors = operatingSystemMXBean.getAvailableProcessors();
+		chars = new char[encryptionKeyLength];
+		Arrays.fill(chars, '1');
+		encryptionKey = new String(chars);
 
-		long freeMemory = operatingSystemMXBean.getFreePhysicalMemorySize();
-		long totalMemory = operatingSystemMXBean.getTotalPhysicalMemorySize();
-
-		System.out.println(freeMemory);
-		System.out.println(totalMemory);
-		System.out.println(totalMemory - freeMemory);
-
-		System.out.println(currentCpu);
-		System.out.println(availableProcessors);
-		System.out.println(currentCpu / availableProcessors);
-
+		test(128, plainText);
 	}
 }
